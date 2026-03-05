fe3d0e4162f8ce14971a77f7fd4101fcd575773d
Geoff Lane <geoff@zorched.net>
2026-03-05 15:36:48 -0500

    Update mix.exs with all the things for publishing I hope

--- mix.exs
+++ mix.exs
@@ -1,26 +1,49 @@
 defmodule Vectored.MixProject do
   use Mix.Project
 
+  @version "0.3.4"
+  @source_url "https://github.com/geofflane/vectored"
+
   def project do
     [
       app: :vectored,
-      version: "0.3.4",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
         plt_add_apps: [:xmerl]
       ],
+
+      # Hex
+      description:
+        "A lightweight, extensible Elixir library for generating SVG images programmatically using Erlang's built-in :xmerl.",
+      package: package(),
+
       # Docs
       name: "Vectored",
-      source_url: "https://github.com/geofflane/vectored.git",
-      homepage_url: "https://github.com/geofflane/vectored.git",
-      docs: [
-        # The main page in the docs
-        main: "Vectored",
-        # logo: "path/to/logo.png",
-        extras: ["README.md"]
-      ]
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      },
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE.md)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Vectored",
+      extras: ["README.md", "LICENSE.md"],
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 
@@ -35,7 +58,7 @@ defmodule Vectored.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
     ]
   end
 end
